14f4a720f0b5803d0507908ded9c9b7be8bb830f
suin <parksuin55@gmail.com>
2023-01-04 10:09:48 +0900

    소수찾기
    1. 이중 for문 사용
    2. set 사용

--- 4회차/5.py
+++ 4회차/5.py
@@ -1,2 +1,26 @@
 # 소수찾기
 # https://school.programmers.co.kr/learn/courses/30/lessons/12921
+
+# 내 풀이
+def solution(n):
+    count = 0
+    for i in range(2, n+1):
+        for a in range(2, int(i**(1/2))+1):
+            if i % a == 0:
+                break
+        else:
+            count += 1
+    return count
+
+
+# 다른 사람의 풀이 / 에라토스테네스의 체
+def solution2(n):
+    num = set(range(2, n+1))
+    for i in range(2, int(n**0.5)+1):
+        if i in num:
+            num -= set(range(i*i,n+1,i))
+    return len(num)
+
+
+print(solution(10))
+print(solution(5))
