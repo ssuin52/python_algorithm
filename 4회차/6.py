be070a939efc1dd63e08ddb001134c54ed13ac57
suin <parksuin55@gmail.com>
2023-01-29 22:20:29 +0900

    (python)프로그래머스 4회차 7번 문제풀이 추가

--- 4회차/6.py
+++ 4회차/6.py
@@ -1,2 +1,4 @@
 # 전력망을 둘로 나누기
 # https://school.programmers.co.kr/learn/courses/30/lessons/86971
+
+# def solution(n, wires):
--- 4회차/7.py
+++ 4회차/7.py
@@ -1,2 +1,16 @@
 # 모음사전
 # https://school.programmers.co.kr/learn/courses/30/lessons/84512
+
+from itertools import product
+
+
+def solution(word):
+    lst = []
+    for j in range(1,6):
+        for i in list(product(['A','E','I','O','U'], repeat=j)):
+            lst.append(''.join(i))
+    lst.sort()
+    return lst.index(word)+1
+
+print(solution('A'))
+
