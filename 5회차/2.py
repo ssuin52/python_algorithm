6f43cc24d6cd046d7a8cec0f7a8a7e3e3fb96065
suin <parksuin55@gmail.com>
2023-02-02 09:34:52 +0900

    (python)프로그래머스 5회차 2번 문제풀이 추가

---
+++ 5회차/2.py
@@ -0,0 +1,31 @@
+# 게임 맵 최단거리
+# https://school.programmers.co.kr/learn/courses/30/lessons/1844
+
+from collections import deque
+
+def solution(maps):
+    # 상하좌우
+    x_move = [1, 0, -1, 0]
+    y_move = [0, 1, 0, -1]
+
+    x_h, y_h = (len(maps[0]), len(maps))
+    queue = deque([(0, 0, 1)])
+
+    while queue:
+        x, y, d = queue.popleft()
+
+        for i in range(4):
+            nx = x + x_move[i]
+            ny = y + y_move[i]
+
+            if nx > -1 and ny > -1 and nx < x_h and ny < y_h:
+                if maps[ny][nx] == 1 or maps[ny][nx] > d + 1:
+                    maps[ny][nx] = d + 1
+                    if nx == x_h - 1 and ny == y_h - 1:
+                        return d + 1
+
+                    queue.append((nx, ny, d + 1))
+    return -1
+
+
+print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
